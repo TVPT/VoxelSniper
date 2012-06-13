a2095e9934327f7ab4b8840421462168765ed317
Nicolas Ristock <nic_ristock@live.de>
2012-06-13 14:47:44 +0200

    * added MD5 hash helper class * added password protection to punish brush (ask Monofraps or MikeMatrix for password) * added kill punishment to punish brush * punish brush is now LivingEntity based

--- src/main/java/com/thevoxelbox/voxelsniper/brush/Punish.java
+++ src/main/java/com/thevoxelbox/voxelsniper/brush/Punish.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 import com.thevoxelbox.voxelsniper.vData;
 import com.thevoxelbox.voxelsniper.vMessage;
 import com.thevoxelbox.voxelsniper.brush.perform.PerformBrush;
+import com.thevoxelbox.voxelsniper.util.HashHelperMD5;
 
 /**
  * 
@@ -21,10 +22,11 @@ import com.thevoxelbox.voxelsniper.brush.perform.PerformBrush;
 public class Punish extends PerformBrush {
 
 	private enum Punishment {
-		FIRE, LIGHTNING, BLINDNESS, DRUNK
+		FIRE, LIGHTNING, BLINDNESS, DRUNK, KILL
 	};
 
 	private Punishment punishment = Punishment.FIRE;
+	private boolean passCorrect = false;
 
 	public Punish() {
 		name = "Punish";
@@ -33,15 +35,20 @@ public class Punish extends PerformBrush {
 	// arrow applies a punishment
 	@Override
 	protected void arrow(vData v) {
+		if(!passCorrect) {
+			v.sendMessage("Y U don't know how to use this brush?!");
+			return;
+		}
+		
 		int _brushSizeSquare = v.brushSize * v.brushSize;
 		Location targetLocation = new Location(v.getWorld(), tb.getX(), tb.getY(), tb.getZ());
 
-		List<Player> _players = v.getWorld().getPlayers();
+		List<LivingEntity> _entities = v.getWorld().getLivingEntities();
 
-		for (Player _p : _players) {
-			if (v.owner().p != _p) {
-				if (_p.getLocation().distanceSquared(targetLocation) < _brushSizeSquare) {
-					applyPunishment(_p);
+		for (LivingEntity _e : _entities) {
+			if (v.owner().p != _e) {
+				if (_e.getLocation().distanceSquared(targetLocation) < _brushSizeSquare) {
+					applyPunishment(_e);
 				}
 			}
 		}
@@ -50,17 +57,22 @@ public class Punish extends PerformBrush {
 	// gunpowder removes all the punishments
 	@Override
 	protected void powder(vData v) {
+		if(!passCorrect) {
+			v.sendMessage("Y U don't know how to use this brush?!");
+			return;
+		}
+		
 		int _brushSizeSquare = v.brushSize * v.brushSize;
 		Location targetLocation = new Location(v.getWorld(), tb.getX(), tb.getY(), tb.getZ());
 
-		List<Player> _players = v.getWorld().getPlayers();
+		List<LivingEntity> _entities = v.getWorld().getLivingEntities();
 
-		for (Player _p : _players) {
-			if (v.owner().p != _p) {
-				if (_p.getLocation().distanceSquared(targetLocation) < _brushSizeSquare) {
-					_p.setFireTicks(0);
-					_p.removePotionEffect(PotionEffectType.BLINDNESS);
-					_p.removePotionEffect(PotionEffectType.CONFUSION);
+		for (LivingEntity _e : _entities) {
+			if (v.owner().p != _e) {
+				if (_e.getLocation().distanceSquared(targetLocation) < _brushSizeSquare) {
+					_e.setFireTicks(0);
+					_e.removePotionEffect(PotionEffectType.BLINDNESS);
+					_e.removePotionEffect(PotionEffectType.CONFUSION);
 				}
 			}
 
@@ -78,18 +90,35 @@ public class Punish extends PerformBrush {
 			if (par[x].equalsIgnoreCase("fire")) {
 				punishment = Punishment.FIRE;
 				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
+				continue;
 			}
 			if (par[x].equalsIgnoreCase("lightning")) {
 				punishment = Punishment.LIGHTNING;
 				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
+				continue;
 			}
 			if (par[x].equalsIgnoreCase("blind")) {
 				punishment = Punishment.BLINDNESS;
 				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
+				continue;
 			}
 			if (par[x].equalsIgnoreCase("drunk")) {
 				punishment = Punishment.DRUNK;
 				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
+				continue;
+			}
+			if(par[x].equalsIgnoreCase("kill")) {
+				punishment = Punishment.KILL;
+				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
+				continue;
+			}
+			if (par[x].startsWith("U")) {
+				// ask Monofraps or MikeMatrix for password
+				if (HashHelperMD5.hash(par[x]).equals("56cd6fa410cd1f05db4f25abaeacb0bf")) {
+					passCorrect = true;
+					v.sendMessage("Punish brush enabled!");
+				}
+				continue;
 			}
 		}
 	}
@@ -101,19 +130,22 @@ public class Punish extends PerformBrush {
 		vm.size();
 	}
 
-	private void applyPunishment(Player p) {
+	private void applyPunishment(LivingEntity e) {		
 		switch (punishment) {
 		case FIRE:
-			p.setFireTicks(20 * 60);
+			e.setFireTicks(20 * 60);
 			break;
 		case LIGHTNING:
-			p.getWorld().strikeLightning(p.getLocation());
+			e.getWorld().strikeLightning(e.getLocation());
 			break;
 		case BLINDNESS:
-			p.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20 * 60, 10), true);
+			e.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20 * 60, 10), true);
 			break;
 		case DRUNK:
-			p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 20 * 60, 10), true);
+			e.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 20 * 60, 10), true);
+			break;
+		case KILL:
+			e.damage(5000000);
 			break;
 		default:
 			Bukkit.getLogger().warning("Could not determine the punishment of punish brush!");
---
+++ src/main/java/com/thevoxelbox/voxelsniper/util/HashHelperMD5.java
@@ -0,0 +1,40 @@
+package com.thevoxelbox.voxelsniper.util;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public final class HashHelperMD5 {
+
+	/**
+	 * Creates MD5 hash of a given string.
+	 * 
+	 * @param str
+	 *            The string to hash
+	 * @return the hash
+	 */
+	public final static String hash(String str) {
+	
+		MessageDigest _md5 = null;
+		StringBuffer _sbMD5sum = new StringBuffer();
+		byte[] _digest = null;
+		
+		try {
+			_md5 = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException _ex) {
+			_ex.printStackTrace();
+			return "";
+		}
+		
+		_md5.reset();
+		_md5.update(str.getBytes());
+		str = "";
+		
+		_digest = _md5.digest();
+		
+		for (byte b : _digest) {
+			_sbMD5sum.append(Integer.toHexString((b & 0xFF) | 0x100).toLowerCase().substring(1, 3));
+		}		
+		
+		return _sbMD5sum.toString();
+	}
+}
