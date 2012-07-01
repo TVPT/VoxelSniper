ef45c21abc8fbb1e3176f3ae8f3bd01fce3b2d32
Monofraps <nic_ristock@live.de>
2012-07-01 16:20:25 +0200

    + added random tp and all-potion punishments * cleaned up punish source a bit

--- src/main/java/com/thevoxelbox/voxelsniper/brush/Punish.java
+++ src/main/java/com/thevoxelbox/voxelsniper/brush/Punish.java
@@ -1,6 +1,7 @@
 package com.thevoxelbox.voxelsniper.brush;
 
 import java.util.List;
+import java.util.Random;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -17,13 +18,17 @@ import com.thevoxelbox.voxelsniper.util.HashHelperMD5;
 
 /**
  * 
- * @author Monofraps, additions by Deamon
+ * @author Monofraps
+ * @author Deamon
  * 
  */
 public class Punish extends PerformBrush {
 
 	private enum Punishment {
-		FIRE, LIGHTNING, BLINDNESS, DRUNK, KILL, INVERT, JUMP
+		// Monofraps
+		FIRE, LIGHTNING, BLINDNESS, DRUNK, KILL, RANDOM_TP, ALL_POTION,
+		// Deamon
+		INVERT, JUMP
 	};
 
 	private Punishment punishment = Punishment.FIRE;
@@ -60,46 +65,33 @@ public class Punish extends PerformBrush {
 		}
 
 		int _brushSizeSquare = v.brushSize * v.brushSize;
-		Location targetLocation = new Location(v.getWorld(), tb.getX(), tb.getY(), tb.getZ());
+		Location _targetLocation = new Location(v.getWorld(), tb.getX(), tb.getY(), tb.getZ());
 
 		List<LivingEntity> _entities = v.getWorld().getLivingEntities();
-		int tmp = 0;
+		int _numPunishApps = 0;
 		for (LivingEntity _e : _entities) {
 			if (v.owner().p != _e) {
 				if(v.brushSize >= 0) {
 					try{
-						if (_e.getLocation().distanceSquared(targetLocation) < _brushSizeSquare) {
-							tmp++;
+						if (_e.getLocation().distanceSquared(_targetLocation) <= _brushSizeSquare) {
+							_numPunishApps++;
 							applyPunishment(_e);
 						}
 					}catch(Exception e) {}
 				} else if(v.brushSize == -3){
-					tmp++;
+					_numPunishApps++;
 					applyPunishment(_e);
 				}
 			}
 		}
-		v.owner().p.sendMessage(ChatColor.DARK_RED + "Punishment applied to " + tmp + " player(s)");
+		v.sendMessage(ChatColor.DARK_RED + "Punishment applied to " + _numPunishApps + " player(s)");
 	}
 
-	// gunpowder removes all the punishments
+	// gunpowder removes all the punishments (except for randomtp)
 	@Override
 	protected void powder(vData v) {
 		if (!passCorrect) {
 			v.sendMessage("Y U don't know how to use this brush?!");
-			return;
-		}
-
-		potionDuration = v.voxelId;
-
-		if (specificPlayer) {
-			Player _punPlay = Bukkit.getPlayer(punishPlayerName);
-			if (_punPlay == null) {
-				v.sendMessage("No player " + punishPlayerName + " found.");
-				return;
-			}
-
-			applyPunishment(_punPlay);
 			return;
 		}
 
@@ -109,7 +101,6 @@ public class Punish extends PerformBrush {
 		List<LivingEntity> _entities = v.getWorld().getLivingEntities();
 
 		for (LivingEntity _e : _entities) {
-			if (v.owner().p != _e) {
 				if (_e.getLocation().distanceSquared(targetLocation) < _brushSizeSquare) {
 					_e.setFireTicks(0);
 					_e.removePotionEffect(PotionEffectType.BLINDNESS);
@@ -117,8 +108,6 @@ public class Punish extends PerformBrush {
 					_e.removePotionEffect(PotionEffectType.SLOW);
 					_e.removePotionEffect(PotionEffectType.JUMP);
 				}
-			}
-
 		}
 
 	}
@@ -126,12 +115,13 @@ public class Punish extends PerformBrush {
 	public void parameters(String[] par, vData v) {
 		if (par[1].equalsIgnoreCase("info")) {
 			v.sendMessage(ChatColor.GOLD + "Punish brush parameters:");
-			v.sendMessage(ChatColor.WHITE + "Type of punishment: fire, lightning, blind, drunk, jump and invert");
-			v.sendMessage(ChatColor.WHITE + "blind and drunk accept a level paramter: /b punish blind:[levelHere]");
+			v.sendMessage(ChatColor.WHITE + "Type of punishment: fire, lightning, blind, drunk, jump, randomtp, kill and invert");
+			v.sendMessage(ChatColor.WHITE + "Some punishments accept a level paramter: /b punish blind:[levelHere]");
 			v.sendMessage(ChatColor.WHITE + "The ID of your voxel material will be used for potion/fire effect duration (seconds).");
 			return;
 		}
-		for (int x = 1; x < par.length; x++) {
+		for (int x = 1; x < par.length; x++) {			
+			// non-potion punishments
 			if (par[x].equalsIgnoreCase("fire")) {
 				punishment = Punishment.FIRE;
 				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
@@ -142,61 +132,9 @@ public class Punish extends PerformBrush {
 				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
 				continue;
 			}
-			if (par[x].toLowerCase().startsWith("blind")) {
-				punishment = Punishment.BLINDNESS;
-				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
-
-				if (par[x].contains(":")) {
-					try {
-						potionLevel = Integer.valueOf(par[x].substring(par[x].indexOf(":") + 1));
-						v.sendMessage(ChatColor.GREEN + "Potion level was set to " + String.valueOf(potionLevel));
-					} catch (Exception _e) {
-						v.sendMessage(ChatColor.RED + "Unable to set potion level. (punishment:potionLevel)");
-					}
-				}
-				continue;
-			}
-			if (par[x].toLowerCase().startsWith("jump")) {
-				punishment = Punishment.JUMP;		
-				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
-				
-				if(par[x].contains(":")) {
-					try {
-					potionLevel = Integer.valueOf(par[x].substring(par[x].indexOf(":") + 1));
-					v.sendMessage(ChatColor.GREEN + "Potion level was set to " + String.valueOf(potionLevel));
-					}
-					catch (Exception _e) {
-						v.sendMessage(ChatColor.RED + "Unable to set potion level. (punishment:potionLevel)");
-					}
-				}				
-				continue;
-			}
-			if (par[x].toLowerCase().startsWith("invert")) {
-				punishment = Punishment.INVERT;
-				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
-
-				if (par[x].contains(":")) {
-					try {
-						potionLevel = Integer.valueOf(par[x].substring(par[x].indexOf(":") + 1));
-						v.sendMessage(ChatColor.GREEN + "Potion level was set to " + String.valueOf(potionLevel));
-					} catch (Exception _e) {
-						v.sendMessage(ChatColor.RED + "Unable to set potion level. (punishment:potionLevel)");
-					}
-				}
-				continue;
-			}
-			if (par[x].toLowerCase().startsWith("drunk")) {
-				punishment = Punishment.DRUNK;
-				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
-
-				if (par[x].contains(":")) {
-					try {
-						potionLevel = Integer.valueOf(par[x].substring(par[x].indexOf(":") + 1));
-						v.sendMessage(ChatColor.GREEN + "Potion level was set to " + String.valueOf(potionLevel));
-					} catch (Exception _e) {
-						v.sendMessage(ChatColor.RED + "Unable to set potion level. (punishment:potionLevel)");
-					}
-				}
+			if (par[x].equalsIgnoreCase("randomtp")) {
+				punishment = Punishment.RANDOM_TP;
+				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());				
 				continue;
 			}
 			if (par[x].equalsIgnoreCase("kill")) {
@@ -204,14 +142,51 @@ public class Punish extends PerformBrush {
 				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
 				continue;
 			}
+			if(par[x].equalsIgnoreCase("allpotion")) {
+				punishment = Punishment.ALL_POTION;
+				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
+				
+				extractPotionLevel(par[x], v);
+				continue;
+			}
+			
+			// potion punishments
+			if (par[x].toLowerCase().startsWith("blind")) {
+				punishment = Punishment.BLINDNESS;
+				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
+
+				extractPotionLevel(par[x], v);
+				continue;
+			}
+			if (par[x].toLowerCase().startsWith("jump")) {
+				punishment = Punishment.JUMP;		
+				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
+				
+				extractPotionLevel(par[x], v);
+				continue;
+			}
+			if (par[x].toLowerCase().startsWith("invert")) {
+				punishment = Punishment.INVERT;
+				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
+
+				extractPotionLevel(par[x], v);
+				continue;
+			}
+			if (par[x].toLowerCase().startsWith("drunk")) {
+				punishment = Punishment.DRUNK;
+				v.sendMessage(ChatColor.GREEN + "Punishment: " + punishment.toString());
+
+				extractPotionLevel(par[x], v);
+				continue;
+			}	
+			
+			
 			if (par[x].startsWith("j")) {
 				// ask Monofraps, MikeMatrix or Deamon for password
 				if (HashHelperMD5.hash(par[x]).equals("440b95f37c4b0009562032974f8cd1e1")) {
 					passCorrect = true;
 					v.sendMessage("Punish brush enabled!");
 				}
-				// do not continue, since U*** could also be a name for SM mode
-				//continue;
 			}
 			if (par[x].equalsIgnoreCase("-toggleSM")) {
 				specificPlayer = !specificPlayer;
@@ -224,7 +199,9 @@ public class Punish extends PerformBrush {
 					}
 				}
 			}
+			
 		}
+		
 	}
 
 	@Override
@@ -232,6 +209,17 @@ public class Punish extends PerformBrush {
 		vm.brushName(name);
 		vm.custom(ChatColor.GREEN + "Punishment: " + punishment.toString());
 		vm.size();
+	}
+	
+	private void extractPotionLevel(String _input, vData v) {
+		if (_input.contains(":")) {
+			try {
+				potionLevel = Integer.valueOf(_input.substring(_input.indexOf(":") + 1));
+				v.sendMessage(ChatColor.GREEN + "Potion level was set to " + String.valueOf(potionLevel));
+			} catch (Exception _e) {
+				v.sendMessage(ChatColor.RED + "Unable to set potion level. (punishment:potionLevel)");
+			}
+		}
 	}
 
 	private void applyPunishment(LivingEntity e) {
@@ -257,6 +245,18 @@ public class Punish extends PerformBrush {
 		case KILL:
 			e.damage(5000000);
 			break;
+		case RANDOM_TP:
+			Random _rand = new Random();			
+			Location _targetLocation = e.getLocation();
+			_targetLocation.setX(_targetLocation.getX() + (_rand.nextInt(400) - 200));
+			_targetLocation.setZ(_targetLocation.getZ() + (_rand.nextInt(400) - 200));
+			e.teleport(_targetLocation);
+			break;
+		case ALL_POTION:
+			e.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 20 * potionDuration, potionLevel), true);
+			e.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 20 * potionDuration, potionLevel), true);
+			e.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 20 * potionDuration, potionLevel), true);
+			e.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 20 * potionDuration, potionLevel), true);
 		default:
 			Bukkit.getLogger().warning("Could not determine the punishment of punish brush!");
 		}
