5665cc3d729bede2d0d77ed7fe6cd71d3efc1187
MikeMatrix <m.braun92@gmail.com>
2012-07-12 22:07:17 +0200

    * Changed Heatray by recoding it and fixed undo bug.

--- src/main/java/com/thevoxelbox/voxelsniper/brush/HeatRay.java
+++ src/main/java/com/thevoxelbox/voxelsniper/brush/HeatRay.java
@@ -7,14 +7,51 @@ package com.thevoxelbox.voxelsniper.brush;
 import com.thevoxelbox.voxelsniper.undo.vUndo;
 import com.thevoxelbox.voxelsniper.vData;
 import com.thevoxelbox.voxelsniper.vMessage;
+
+import java.util.ArrayList;
 import java.util.Random;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.util.Vector;
+import org.bukkit.util.noise.SimplexNoiseGenerator;
 
 /**
  * THIS BRUSH SHOULD NOT USE PERFORMERS
- *
+ * 
  * @author Gavjenks (derived from Piotr'w ball replace brush)
  */
 public class HeatRay extends Brush {
+
+    private static final double REQUIRED_OBSIDIAN_DENSITY = 0.6;
+    private static final double REQUIRED_COBBLE_DENSITY = 0.5;
+    private static final double REQUIRED_FIRE_DENSITY = -0.25;
+    private static final double REQUIRED_AIR_DENSITY = 0;
+
+    private static final ArrayList<Material> FLAMABLE_BLOCKS = new ArrayList<Material>();
+
+    private enum FlameableBlock {
+
+        WOOD(Material.WOOD), SAPLING(Material.SAPLING), LOG(Material.LOG), LEAVES(Material.LEAVES), SPONGE(Material.SPONGE), WEB(Material.WEB), LONG_GRASS(
+                Material.LONG_GRASS), DEAD_BUSH(Material.DEAD_BUSH), WOOL(Material.WOOL), YELLOW_FLOWER(Material.YELLOW_FLOWER), RED_ROSE(Material.RED_ROSE), TORCH(
+                Material.TORCH), FIRE(Material.FIRE), WOOD_STAIRS(Material.WOOD_STAIRS), CROPS(Material.CROPS), SIGN_POST(Material.SIGN_POST), WOODEN_DOOR(
+                Material.WOODEN_DOOR), LADDER(Material.LADDER), WALL_SIGN(Material.WALL_SIGN), WOOD_PLATE(Material.WOOD_PLATE), SNOW(Material.SNOW), ICE(
+                Material.ICE), SUGAR_CANE_BLOCK(Material.SUGAR_CANE_BLOCK), FENCE(Material.FENCE), TRAP_DOOR(Material.TRAP_DOOR), VINE(Material.VINE), FENCE_GATE(
+                Material.FENCE_GATE), WATER_LILLY(Material.WATER_LILY);
+
+        public Material material;
+
+        FlameableBlock(Material material) {
+            this.material = material;
+        }
+    }
+
+    static {
+        for (FlameableBlock _flameableBlock : FlameableBlock.values()) {
+            FLAMABLE_BLOCKS.add(_flameableBlock.material);
+        }
+    }
 
     public HeatRay() {
         name = "Heat Ray";
@@ -25,7 +62,7 @@ public class HeatRay extends Brush {
         bx = tb.getX();
         by = tb.getY();
         bz = tb.getZ();
-        HeatRay(v);
+        heatRay(v);
     }
 
     @Override
@@ -33,7 +70,7 @@ public class HeatRay extends Brush {
         bx = tb.getX();
         by = tb.getY();
         bz = tb.getZ();
-        HeatRay(v);
+        heatRay(v);
     }
 
     @Override
@@ -42,128 +79,78 @@ public class HeatRay extends Brush {
         vm.size();
     }
 
-    public void HeatRay(vData v) {
+    public void heatRay(vData v) {
+        SimplexNoiseGenerator _airGenerator = new SimplexNoiseGenerator(new Random());
+        SimplexNoiseGenerator _fireGenerator = new SimplexNoiseGenerator(new Random());
+        SimplexNoiseGenerator _cobbleGenerator = new SimplexNoiseGenerator(new Random());
+        SimplexNoiseGenerator _obsidianGenerator = new SimplexNoiseGenerator(new Random());
 
-        //This is designed as a purely entertaining brush to use to destroy things you hate in a dramatic fashion.  E.g. Daro builds.  Just using a /b br with fire is not nearly as much fun as this:
-        //Basically burns anything that would seem flammable in real life, smelts stone-like things into stone (or 10% chance of destroyign outright or charring into obsidian), kills plants, turns grass to dirt, evaporates water and snow and ice, turns sandy things to glass.
-        //All the stuff one would expect from a sphere of intense heat...
+        double _bSquare = (v.brushSize + 0.5) * (v.brushSize + 0.5);
 
-        int bsize = v.brushSize;
+        Vector _targetLocation = tb.getLocation().toVector();
+        Location _currentLocation = new Location(tb.getWorld(), 0, 0, 0);
+        Block _currentBlock = null;
+        vUndo _undo = new vUndo(tb.getWorld().getName());
 
-        vUndo h = new vUndo(tb.getWorld().getName());
-        int octant = 0;
-        int octX = 0;
-        int octY = 0;
-        int octZ = 0;
-        int r = 0;
-        Random generator = new Random();
-        double bpow = Math.pow(bsize + 0.5, 2);
-        for (int z = bsize; z >= 0; z--) {
-            for (int x = bsize; x >= 0; x--) {
-                for (int y = bsize; y >= 0; y--) {
-                    if ((Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)) <= bpow) {
-                        for (int i = 1; i < 9; i++) { //this just avoids me copying and pasting all those huge bits of code below 8 times.  More compact to look at. -- You should make this a switch() {case(): } block, faster to use
-                            if (i == 1) {
-                                octX = bx + x;
-                                octY = by + z;
-                                octZ = bz + y;
-                            }
-                            if (i == 2) {
-                                octX = bx + x;
-                                octY = by + z;
-                                octZ = bz - y;
-                            }
-                            if (i == 3) {
-                                octX = bx + x;
-                                octY = by - z;
-                                octZ = bz + y;
-                            }
-                            if (i == 4) {
-                                octX = bx + x;
-                                octY = by - z;
-                                octZ = bz - y;
-                            }
-                            if (i == 5) {
-                                octX = bx - x;
-                                octY = by + z;
-                                octZ = bz + y;
-                            }
-                            if (i == 6) {
-                                octX = bx - x;
-                                octY = by + z;
-                                octZ = bz - y;
-                            }
-                            if (i == 7) {
-                                octX = bx - x;
-                                octY = by - z;
-                                octZ = bz + y;
-                            }
-                            if (i == 8) {
-                                octX = bx - x;
-                                octY = by - z;
-                                octZ = bz - y;
-                            }
+        for (int _z = v.brushSize; _z >= -v.brushSize; _z--) {
+            for (int _x = v.brushSize; _x >= -v.brushSize; _x--) {
+                for (int _y = v.brushSize; _y >= -v.brushSize; _y--) {
+                    _currentLocation.setX(bx + _x);
+                    _currentLocation.setY(by + _y);
+                    _currentLocation.setZ(bz + _z);
 
-                            octant = getBlockIdAt(octX, octY, octZ);
+                    if (_currentLocation.toVector().isInSphere(_targetLocation, v.brushSize)) {
+                        _currentBlock = _currentLocation.getBlock();
+                        if (_currentBlock == null || _currentBlock.getType() == Material.CHEST) {
+                            continue;
+                        }
 
-                            //Air with something below it to fire 50%, for sual effect
-                            if (octant == 0 && getBlockIdAt(octX, octY - 1, octZ) != 0) {
-                                r = generator.nextInt(10);
-                                if (r < 6) {
-                                    setBlockIdAt(51, octX, octY, octZ); //no need for undo, since fire will burn out.
+                        if (_currentBlock.isLiquid()) {
+                            _undo.put(_currentBlock);
+                            _currentBlock.setType(Material.AIR);
+                            continue;
+                        }
+
+                        if (FLAMABLE_BLOCKS.contains(_currentBlock.getType())) {
+                            _undo.put(_currentBlock);
+                            _currentBlock.setType(Material.FIRE);
+                            continue;
+                        }
+
+                        if (!_currentBlock.getType().equals(Material.AIR)) {
+                            double _airDensity = _airGenerator.noise(_currentLocation.getX(), _currentLocation.getY(), _currentLocation.getZ());
+                            double _fireDensity = _fireGenerator.noise(_currentLocation.getX(), _currentLocation.getY(), _currentLocation.getZ());
+                            double _cobbleDensity = _cobbleGenerator.noise(_currentLocation.getX(), _currentLocation.getY(), _currentLocation.getZ());
+                            double _obsidianDensity = _obsidianGenerator.noise(_currentLocation.getX(), _currentLocation.getY(), _currentLocation.getZ());
+
+                            if (_obsidianDensity >= REQUIRED_OBSIDIAN_DENSITY) {
+                                if (_currentBlock.getType() != Material.OBSIDIAN) {
+                                    _undo.put(_currentBlock);
+                                    _currentBlock.setType(Material.OBSIDIAN);
+                                }
+                            } else if (_cobbleDensity >= REQUIRED_COBBLE_DENSITY) {
+                                if (_currentBlock.getType() != Material.COBBLESTONE) {
+                                    _undo.put(_currentBlock);
+                                    _currentBlock.setType(Material.COBBLESTONE);
+                                }
+                            } else if (_fireDensity >= REQUIRED_FIRE_DENSITY) {
+                                if (_currentBlock.getType() != Material.FIRE) {
+                                    _undo.put(_currentBlock);
+                                    _currentBlock.setType(Material.FIRE);
+                                }
+                            } else if (_airDensity >= REQUIRED_AIR_DENSITY) {
+                                if (_currentBlock.getType() != Material.AIR) {
+                                    _undo.put(_currentBlock);
+                                    _currentBlock.setType(Material.AIR);
                                 }
                             }
+                        }
+                    }
 
-                            //flammable stuff to fire
-                            if (octant == 81 || octant == 83 || octant == 92 || octant == 76 || octant == 75 || octant == 84 || octant == 85 || octant == 86 || octant == 91 || octant == 5 || octant == 6 || octant == 17 || octant == 18 || octant == 19 || octant == 25 || octant == 26 || octant == 35 || octant == 27 || octant == 28 || octant == 35 || octant == 39 || octant == 40 || octant == 47 || octant == 50 || octant == 53 || octant == 54 || octant == 55 || octant == 58 || octant == 59 || octant == 63 || octant == 64 || octant == 65 || octant == 66 || octant == 68 || octant == 69 || octant == 72 || octant == 44) {
-                                h.put(clampY(octX, octY, octZ));
-                                setBlockIdAt(51, octX, octY, octZ);
-                            }
-                            //meltable things to air, without flames
-                            if (octant == 78 || octant == 89 || octant == 71 || octant == 79 || octant == 80 || octant == 56 || octant == 73 || octant == 86 || octant == 91 || octant == 41 || octant == 42 || octant == 20) {
-                                h.put(clampY(octX, octY, octZ));
-                                setBlockIdAt(0, octX, octY, octZ);
-                            }
-                            //grass to dirt
-                            if (octant == 2 || octant == 60) {
-                                h.put(clampY(octX, octY, octZ));
-                                setBlockIdAt(3, octX, octY, octZ);
-                            }
+                }
+            }
+        }
 
-                            //dirt can be destroyed
-                            if (octant == 2) {
-                                r = generator.nextInt(10);
-                                if (r < 4) { //30% chance of destroying dirt
-                                    h.put(clampY(octX, octY, octZ));
-                                    setBlockIdAt(0, octX, octY, octZ);
-                                }
-                            }
-
-                            //evaporate non ocean water
-                            if ((octant == 8 || octant == 9) && octY > 63) {
-                                h.put(clampY(octX, octY, octZ));
-                                setBlockIdAt(0, octX, octY, octZ);
-                            }
-
-                            //stone mats to cobble, air, or obsidian
-                            if (octant == 70 || octant == 12 || octant == 24 || octant == 49 || octant == 1 || octant == 4 || octant == 12 || octant == 24 || octant == 67 || octant == 61 || octant == 62 || octant == 48 || octant == 45 || octant == 43 || octant == 23 || octant == 13 || octant == 7 || octant == 14 || octant == 15 || octant == 16 || octant == 21) {
-                                h.put(clampY(octX, octY, octZ));
-                                r = generator.nextInt(10);
-                                if (r < 2) { //10% chance of destroying stone stuff
-                                    setBlockIdAt(0, octX, octY, octZ);
-                                } else if (r > 2 && r < 5) { //20% chance of turning stone stuff to obsidian (might get rid of if ugly)
-                                    setBlockIdAt(49, octX, octY, octZ);
-                                } else {
-                                    setBlockIdAt(4, octX, octY, octZ);
-                                }
-                            }
-
-                        } //end for loop for 8 octants
-
-                    }//end if for whether it'w in the brush or not.
-                }//Y
-            }//X
-            v.storeUndo(h);
-        }//Z
+        v.storeUndo(_undo);
     }
 }
