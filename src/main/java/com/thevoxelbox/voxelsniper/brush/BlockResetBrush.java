9243e826716d5b03eb9149e7b21ce276e5e3b43f
MikeMatrix <m.braun92@gmail.com>
2012-07-16 05:48:45 +0200

    * Changed Block Reset Brush

--- src/main/java/com/thevoxelbox/voxelsniper/brush/BlockResetBrush.java
+++ src/main/java/com/thevoxelbox/voxelsniper/brush/BlockResetBrush.java
@@ -1,5 +1,8 @@
 package com.thevoxelbox.voxelsniper.brush;
 
+import java.util.ArrayList;
+
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 
 import com.thevoxelbox.voxelsniper.vData;
@@ -10,6 +13,12 @@ import com.thevoxelbox.voxelsniper.vMessage;
  * 
  */
 public class BlockResetBrush extends Brush {
+
+    private static final ArrayList<Material> DENIED_UPDATES = new ArrayList<Material>();
+
+    static {
+        DENIED_UPDATES.add(Material.SIGN);
+    }
 
     /**
      * 
@@ -34,9 +43,12 @@ public class BlockResetBrush extends Brush {
             for (int _x = -v.brushSize; _x <= v.brushSize; _x++) {
                 for (int _y = -v.brushSize; _y <= v.brushSize; _y++) {
                     Block _block = w.getBlockAt(bx + _x, by + _y, bz + _z);
+                    if (DENIED_UPDATES.contains(_block.getType())) {
+                        continue;
+                    }
                     byte _oldData = _block.getData();
                     _block.setTypeIdAndData(_block.getTypeId(), (byte) ((_block.getData() + 1) & 0xf), true);
-                    _block.setTypeIdAndData(_block.getTypeId(), _oldData, false);
+                    _block.setTypeIdAndData(_block.getTypeId(), _oldData, true);
                 }
             }
         }
@@ -44,20 +56,6 @@ public class BlockResetBrush extends Brush {
 
     @Override
     protected final void powder(final vData v) {
-        w = tb.getWorld();
-        bx = tb.getX();
-        by = tb.getY();
-        bz = tb.getZ();
-
-        for (int _z = -v.brushSize; _z <= v.brushSize; _z++) {
-            for (int _x = -v.brushSize; _x <= v.brushSize; _x++) {
-                for (int _y = -v.brushSize; _y <= v.brushSize; _y++) {
-                    Block _block = w.getBlockAt(bx + _x, by + _y, bz + _z);
-                    byte _oldData = _block.getData();
-                    _block.setTypeIdAndData(_block.getTypeId(), (byte) ((_block.getData() + 1) & 0xf), true);
-                    _block.setTypeIdAndData(_block.getTypeId(), _oldData, true);
-                }
-            }
-        }
+        this.arrow(v);
     }
 }
