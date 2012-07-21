d58b5133dd3898a146428aa208143cc915ded0db
MikeMatrix <m.braun92@gmail.com>
2012-07-21 15:19:21 +0200

    * Fixed undo caching bug in disk when performer is 'c'. * Changed disk brush to match coding policies.

--- src/main/java/com/thevoxelbox/voxelsniper/brush/Disc.java
+++ src/main/java/com/thevoxelbox/voxelsniper/brush/Disc.java
@@ -1,62 +1,72 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.thevoxelbox.voxelsniper.brush;
 
-import com.thevoxelbox.voxelsniper.brush.perform.PerformBrush;
+import org.bukkit.ChatColor;
+import org.bukkit.util.Vector;
+
 import com.thevoxelbox.voxelsniper.vData;
 import com.thevoxelbox.voxelsniper.vMessage;
-import org.bukkit.ChatColor;
+import com.thevoxelbox.voxelsniper.brush.perform.PerformBrush;
 
 /**
- *
+ * 
  * @author Voxel
  */
 public class Disc extends PerformBrush {
 
+    private double trueCircle = 0;
+
+    /**
+     * Default Constructor.
+     */
     public Disc() {
-        name = "Disc";
+        this.name = "Disc";
+    }
+
+    /**
+     * Disc executor.
+     * 
+     * @param v
+     */
+    public final void disc(final vData v) {
+        final double _radiusSquared = (v.brushSize + this.trueCircle) * (v.brushSize + this.trueCircle);
+        final Vector _centerPoint = this.tb.getLocation().toVector();
+        final Vector _currentPoint = _centerPoint.clone();
+
+        for (int _x = -v.brushSize; _x <= v.brushSize; _x++) {
+            _currentPoint.setX(_centerPoint.getX() + _x);
+            for (int _z = -v.brushSize; _z <= v.brushSize; _z++) {
+                _currentPoint.setZ(_centerPoint.getZ() + _z);
+                if (_centerPoint.distanceSquared(_currentPoint) <= _radiusSquared) {
+                    this.current.perform(this.clampY(_currentPoint.getBlockX(), _currentPoint.getBlockY(), _currentPoint.getBlockZ()));
+                }
+            }
+        }
+        v.storeUndo(this.current.getUndo());
     }
 
     @Override
-    protected void arrow(com.thevoxelbox.voxelsniper.vData v) {
-        bx = tb.getX();
-        by = tb.getY();
-        bz = tb.getZ();
-        disc(v);
-    }
-
-    @Override
-    protected void powder(com.thevoxelbox.voxelsniper.vData v) {
-        bx = lb.getX();
-        by = lb.getY();
-        bz = lb.getZ();
-        disc(v);
-    }
-
-    @Override
-    public void info(vMessage vm) {
-        vm.brushName(name);
+    public final void info(final vMessage vm) {
+        vm.brushName(this.name);
         vm.size();
-        //vm.voxel();
+        // vm.voxel();
     }
-    double trueCircle = 0;
 
     @Override
-    public void parameters(String[] par, com.thevoxelbox.voxelsniper.vData v) {
+    public final void parameters(final String[] par, final vData v) {
         if (par[1].equalsIgnoreCase("info")) {
             v.sendMessage(ChatColor.GOLD + "Disc Brush Parameters:");
-            v.sendMessage(ChatColor.AQUA + "/b d true -- will use a true circle algorithm instead of the skinnier version with classic sniper nubs. /b b false will switch back. (false is default)");
+            v.sendMessage(ChatColor.AQUA + "/b d true|false"
+                    + " -- toggles useing the true circle algorithm instead of the skinnier version with classic sniper nubs. (false is default)");
             return;
         }
-        for (int x = 1; x < par.length; x++) {
-            if (par[x].startsWith("true")) {
-                trueCircle = 0.5;
+        for (int _x = 1; _x < par.length; _x++) {
+            final String _string = par[_x].toLowerCase();
+            if (_string.startsWith("true")) {
+                this.trueCircle = 0.5;
                 v.sendMessage(ChatColor.AQUA + "True circle mode ON.");
                 continue;
-            } else if (par[x].startsWith("false")) {
-                trueCircle = 0;
+            } else if (_string.startsWith("false")) {
+                this.trueCircle = 0;
                 v.sendMessage(ChatColor.AQUA + "True circle mode OFF.");
                 continue;
             } else {
@@ -65,28 +75,19 @@ public class Disc extends PerformBrush {
         }
     }
 
-    public void disc(vData v) {
-        int bsize = v.brushSize;
-        double bpow = Math.pow(bsize + trueCircle, 2);
-        current.perform(clampY(bx, by, bz));
-        for (int x = bsize; x >= 0; x--) {
-            current.perform(clampY(bx + x, by, bz));
-            current.perform(clampY(bx - x, by, bz));
-            current.perform(clampY(bx, by, bz + x));
-            current.perform(clampY(bx, by, bz - x));
-        }
-        for (int x = bsize; x >= 1; x--) {
-            double xpow = Math.pow(x, 2);
-            for (int y = bsize; y >= 1; y--) {
-                if ((xpow + Math.pow(y, 2)) <= bpow) {
-                    current.perform(clampY(bx + x, by, bz + y));
-                    current.perform(clampY(bx + x, by, bz - y));
-                    current.perform(clampY(bx - x, by, bz + y));
-                    current.perform(clampY(bx - x, by, bz - y));
-                }
-            }
-        }
+    @Override
+    protected final void arrow(final vData v) {
+        this.bx = this.tb.getX();
+        this.by = this.tb.getY();
+        this.bz = this.tb.getZ();
+        this.disc(v);
+    }
 
-        v.storeUndo(current.getUndo());
+    @Override
+    protected final void powder(final vData v) {
+        this.bx = this.lb.getX();
+        this.by = this.lb.getY();
+        this.bz = this.lb.getZ();
+        this.disc(v);
     }
 }
