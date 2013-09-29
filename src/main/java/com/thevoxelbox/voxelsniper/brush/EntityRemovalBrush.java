7129e3b88095b78b7e9e7c29e5c9525a56c37734
Michael Braun <m.braun92@gmail.com>
2013-09-29 19:18:55 +0200

    * Refactored Entity Removal Brush to be closer to the code standards * Removed any NMS dependencies in Entity Removal Brush * Added all Hanging entities to the exclusion list of the Entity Removal Brush

--- src/main/java/com/thevoxelbox/voxelsniper/brush/EntityRemovalBrush.java
+++ src/main/java/com/thevoxelbox/voxelsniper/brush/EntityRemovalBrush.java
@@ -2,18 +2,15 @@ package com.thevoxelbox.voxelsniper.brush;
 
 import com.thevoxelbox.voxelsniper.Message;
 import com.thevoxelbox.voxelsniper.SnipeData;
-import net.minecraft.server.v1_6_R3.EntityCreature;
-import net.minecraft.server.v1_6_R3.NPC;
 import org.bukkit.ChatColor;
 import org.bukkit.Chunk;
-import org.bukkit.craftbukkit.v1_6_R3.entity.CraftEntity;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.ItemFrame;
-import org.bukkit.entity.Painting;
+import org.bukkit.entity.Hanging;
+import org.bukkit.entity.NPC;
 import org.bukkit.entity.Player;
 
 /**
- * @author Voxel
+ *
  */
 public class EntityRemovalBrush extends Brush
 {
@@ -27,75 +24,69 @@ public class EntityRemovalBrush extends Brush
         this.setName("Entity Removal");
     }
 
-    private void radialRemoval(final SnipeData v)
+    private void radialRemoval(SnipeData v)
     {
-        final Chunk targetChunk = this.getTargetBlock().getChunk();
+        final Chunk targetChunk = getTargetBlock().getChunk();
         int entityCount = 0;
         int chunkCount = 0;
 
-        entityCount += this.removeEntities(targetChunk);
+        entityCount += removeEntities(targetChunk);
 
         for (int x = targetChunk.getX() - v.getBrushSize(); x <= targetChunk.getX() + v.getBrushSize(); x++)
         {
             for (int z = targetChunk.getZ() - v.getBrushSize(); z <= targetChunk.getZ() + v.getBrushSize(); z++)
             {
-                entityCount += removeEntities(this.getWorld().getChunkAt(x, z));
+                entityCount += removeEntities(getWorld().getChunkAt(x, z));
                 chunkCount++;
             }
         }
         v.sendMessage(ChatColor.GREEN + "Removed " + ChatColor.RED + entityCount + ChatColor.GREEN + " entities out of " + ChatColor.BLUE + chunkCount + ChatColor.GREEN + " chunks.");
     }
 
-    private int removeEntities(final Chunk chunk)
+    private int removeEntities(Chunk chunk)
     {
         int entityCount = 0;
 
-        for (final Entity entity : chunk.getEntities())
+        for (Entity entity : chunk.getEntities())
         {
-            if ((!(entity instanceof Player)) && (!(entity instanceof Painting)) && (!(entity instanceof ItemFrame)))
+            if (entity instanceof Player || entity instanceof Hanging || entity instanceof NPC)
             {
-                if (((CraftEntity) entity).getHandle() instanceof NPC)
-                {
-                    if (!(((CraftEntity) entity).getHandle() instanceof EntityCreature))
-                    {
-                        continue;
-                    }
-                }
-                entity.remove();
-                entityCount++;
+                continue;
             }
+            entity.remove();
+            entityCount++;
         }
 
         return entityCount;
     }
 
     @Override
-    protected final void arrow(final SnipeData v)
+    protected void arrow(SnipeData v)
     {
         this.radialRemoval(v);
     }
 
     @Override
-    protected final void powder(final SnipeData v)
+    protected void powder(SnipeData v)
     {
         this.radialRemoval(v);
     }
 
     @Override
-    public final void info(final Message vm)
+    public void info(Message vm)
     {
-        vm.brushName(this.getName());
+        vm.brushName(getName());
         vm.size();
     }
 
     @Override
-    public final int getTimesUsed()
+    public int getTimesUsed()
     {
         return EntityRemovalBrush.timesUsed;
     }
 
     @Override
-    public final void setTimesUsed(final int tUsed)
+    public void setTimesUsed(int tUsed)
     {
         EntityRemovalBrush.timesUsed = tUsed;
     }
