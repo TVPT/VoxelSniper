407999649a4c8c2d70aa3c081c2f43c082e54020
lukecfairchild <lukecfairchild@gmail.com>
2013-12-11 15:47:29 -0800

    Update VoxelSniperListener.java
    forgot to import EventPriority

--- src/main/java/com/thevoxelbox/voxelsniper/VoxelSniperListener.java
+++ src/main/java/com/thevoxelbox/voxelsniper/VoxelSniperListener.java
@@ -8,6 +8,7 @@ import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
