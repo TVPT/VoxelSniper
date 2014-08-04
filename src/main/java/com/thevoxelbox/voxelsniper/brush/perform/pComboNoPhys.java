1198864686ab13475c06ec7138092a8d5bd9eed9
thepowderguy <brandonli2010@gmail.com>
2014-08-04 11:41:57 -0400

    Update pComboNoPhys.java

--- src/main/java/com/thevoxelbox/voxelsniper/brush/perform/pComboNoPhys.java
+++ src/main/java/com/thevoxelbox/voxelsniper/brush/perform/pComboNoPhys.java
@@ -36,6 +36,7 @@ public class pComboNoPhys extends vPerformer
         w = v.getWorld();
         i = v.getVoxelId();
         d = v.getData();
+        p = v.owner().getPlayer().getName();
     }
 
     @SuppressWarnings("deprecation")
@@ -43,6 +44,8 @@ public class pComboNoPhys extends vPerformer
     public void perform(Block b)
     {
         h.put(b);
+	CoreProtectUtils.logBlockRemove(b, p);
         b.setTypeIdAndData(i, d, false);
+	CoreProtectUtils.logBlockPlace(b, p);
     }
 }
