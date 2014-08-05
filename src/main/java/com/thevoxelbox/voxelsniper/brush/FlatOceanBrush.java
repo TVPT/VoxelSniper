fd811f63a4766fbdd7d44be6346e5abdaa5666f4
thepowderguy <brandonli2010@gmail.com>
2014-08-05 13:58:20 -0400

    Update for CoreProtect

--- src/main/java/com/thevoxelbox/voxelsniper/brush/FlatOceanBrush.java
+++ src/main/java/com/thevoxelbox/voxelsniper/brush/FlatOceanBrush.java
@@ -2,152 +2,56 @@ package com.thevoxelbox.voxelsniper.brush;
 
 import com.thevoxelbox.voxelsniper.Message;
 import com.thevoxelbox.voxelsniper.SnipeData;
-import com.thevoxelbox.voxelsniper.Undo;
 import com.thevoxelbox.voxelsniper.util.CoreProtectUtils;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Location;
+import org.bukkit.Chunk;
 import org.bukkit.Material;
-import org.bukkit.World;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.TreeSet;
 
 /**
- * Moves a selection blockPositionY a certain amount.
- * http://www.voxelwiki.com/minecraft/Voxelsniper#Move_Brush
- *
- * @author MikeMatrix
+ * @author GavJenks
  */
-public class MoveBrush extends Brush
+public class FlatOceanBrush extends Brush
 {
-    /**
-     * Breakable Blocks to determine if no-physics should be used.
-     */
-    private static final Set<Material> BREAKABLE_MATERIALS = new TreeSet<Material>();
-
-    static
-    {
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.SAPLING);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.BED_BLOCK);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.POWERED_RAIL);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.DETECTOR_RAIL);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.LONG_GRASS);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.DEAD_BUSH);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.PISTON_EXTENSION);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.YELLOW_FLOWER);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.RED_ROSE);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.BROWN_MUSHROOM);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.RED_MUSHROOM);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.TORCH);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.FIRE);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.CROPS);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.SIGN_POST);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.WOODEN_DOOR);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.LADDER);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.RAILS);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.WALL_SIGN);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.LEVER);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.STONE_PLATE);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.IRON_DOOR_BLOCK);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.WOOD_PLATE);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.REDSTONE_TORCH_OFF);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.REDSTONE_TORCH_ON);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.STONE_BUTTON);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.SNOW);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.CACTUS);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.SUGAR_CANE_BLOCK);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.CAKE_BLOCK);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.DIODE_BLOCK_OFF);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.DIODE_BLOCK_ON);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.TRAP_DOOR);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.PUMPKIN_STEM);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.MELON_STEM);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.VINE);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.WATER_LILY);
-        MoveBrush.BREAKABLE_MATERIALS.add(Material.NETHER_WARTS);
-    }
-
-    /**
-     * Saved direction.
-     */
-    private final int[] moveDirections = {0, 0, 0};
-    /**
-     * Saved selection.
-     */
-    private Selection selection = null;
+    private static final int DEFAULT_WATER_LEVEL = 29;
+    private static final int DEFAULT_FLOOR_LEVEL = 8;
+    private int waterLevel = DEFAULT_WATER_LEVEL;
+    private int floorLevel = DEFAULT_FLOOR_LEVEL;
 
     /**
      *
      */
-    public MoveBrush()
+    public FlatOceanBrush()
     {
-        this.setName("Move");
+        this.setName("FlatOcean");
     }
 
-    /**
-     * Moves the given selection blockPositionY the amount given in direction and saves an undo for the player.
-     *
-     * @param v
-     * @param selection
-     * @param direction
-     */
     @SuppressWarnings("deprecation")
-	private void moveSelection(final SnipeData v, final Selection selection, final int[] direction)
+	private void flatOcean(final Chunk chunk, final SnipeData v)
     {
-        if (selection.getBlockStates().size() > 0)
+        for (int x = 0; x < CHUNK_SIZE; x++)
         {
-            final World world = selection.getBlockStates().get(0).getWorld();
-
-            final Undo undo = new Undo();
-            final HashSet<Block> undoSet = new HashSet<Block>();
-
-            final Selection newSelection = new Selection();
-            final Location movedLocation1 = selection.getLocation1();
-            movedLocation1.add(direction[0], direction[1], direction[2]);
-            final Location movedLocation2 = selection.getLocation2();
-            movedLocation2.add(direction[0], direction[1], direction[2]);
-            newSelection.setLocation1(movedLocation1);
-            newSelection.setLocation2(movedLocation2);
-            try
+            for (int z = 0; z < CHUNK_SIZE; z++)
             {
-                newSelection.calculateRegion();
-            }
-            catch (final Exception exception)
-            {
-                v.getVoxelMessage().brushMessage("The new Selection has more blocks than the original selection. This should never happen!");
-            }
-
-            for (final BlockState blockState : selection.getBlockStates())
-            {
-                undoSet.add(blockState.getBlock());
-            }
-            for (final BlockState blockState : newSelection.getBlockStates())
-            {
-                undoSet.add(blockState.getBlock());
-            }
-
-            for (final Block block : undoSet)
-            {
-                undo.put(block);
-            }
-            v.owner().storeUndo(undo);
-
-            for (final BlockState blockState : selection.getBlockStates())
-            {
-                blockState.getBlock().setType(Material.AIR);
-            }
-            for (final BlockState blockState : selection.getBlockStates())
-            {
-                final Block affectedBlock = world.getBlockAt(blockState.getX() + direction[0], blockState.getY() + direction[1], blockState.getZ() + direction[2]);
-                CoreProtectUtils.logBlockRemove(affectedBlock, v.owner().getPlayer().getName());
-                affectedBlock.setTypeId(blockState.getTypeId(), !MoveBrush.BREAKABLE_MATERIALS.contains(blockState.getType()));
-                affectedBlock.setData(blockState.getRawData());
-        	    CoreProtectUtils.logBlockPlace(affectedBlock, v.owner().getPlayer().getName());
+                for (int y = 0; y < chunk.getWorld().getMaxHeight(); y++)
+                {
+                    if (y <= this.floorLevel)
+                    {
+                        chunk.getBlock(x, y, z).setType(Material.DIRT);
+                    }
+                    else if (y <= this.waterLevel)
+                    {
+                        CoreProtectUtils.logBlockRemove(chunk.getBlock(x, y, z), v.owner().getPlayer().getName());
+                        chunk.getBlock(x, y, z).setTypeId(Material.STATIONARY_WATER.getId(), false);
+                	    CoreProtectUtils.logBlockPlace(chunk.getBlock(x, y, z), v.owner().getPlayer().getName());
+                    }
+                    else
+                    {
+                        CoreProtectUtils.logBlockRemove(chunk.getBlock(x, y, z), v.owner().getPlayer().getName());
+                        chunk.getBlock(x, y, z).setTypeId(Material.AIR.getId(), false);
+                	    CoreProtectUtils.logBlockPlace(chunk.getBlock(x, y, z), v.owner().getPlayer().getName());
+                    }
+                }
             }
         }
     }
@@ -155,56 +59,30 @@ public class MoveBrush extends Brush
     @Override
     protected final void arrow(final SnipeData v)
     {
-        if (this.selection == null)
-        {
-            this.selection = new Selection();
-        }
-        this.selection.setLocation1(this.getTargetBlock().getLocation());
-        v.getVoxelMessage().brushMessage("Point 1 set.");
-
-        try
-        {
-            if (this.selection.calculateRegion())
-            {
-                this.moveSelection(v, this.selection, this.moveDirections);
-                this.selection = null;
-            }
-        }
-        catch (final Exception exception)
-        {
-            v.sendMessage(exception.getMessage());
-        }
+        this.flatOcean(this.getWorld().getChunkAt(this.getTargetBlock()), v);
     }
 
     @Override
     protected final void powder(final SnipeData v)
     {
-        if (this.selection == null)
-        {
-            this.selection = new Selection();
-        }
-        this.selection.setLocation2(this.getTargetBlock().getLocation());
-        v.getVoxelMessage().brushMessage("Point 2 set.");
-
-        try
-        {
-            if (this.selection.calculateRegion())
-            {
-                this.moveSelection(v, this.selection, this.moveDirections);
-                this.selection = null;
-            }
-        }
-        catch (final Exception exception)
-        {
-            v.sendMessage(exception.getMessage());
-        }
+        this.flatOcean(this.getWorld().getChunkAt(this.getTargetBlock()), v);
+        this.flatOcean(this.getWorld().getChunkAt(this.clampY(this.getTargetBlock().getX() + CHUNK_SIZE, 1, this.getTargetBlock().getZ())), v);
+        this.flatOcean(this.getWorld().getChunkAt(this.clampY(this.getTargetBlock().getX() + CHUNK_SIZE, 1, this.getTargetBlock().getZ() + CHUNK_SIZE)), v);
+        this.flatOcean(this.getWorld().getChunkAt(this.clampY(this.getTargetBlock().getX(), 1, this.getTargetBlock().getZ() + CHUNK_SIZE)), v);
+        this.flatOcean(this.getWorld().getChunkAt(this.clampY(this.getTargetBlock().getX() - CHUNK_SIZE, 1, this.getTargetBlock().getZ() + CHUNK_SIZE)), v);
+        this.flatOcean(this.getWorld().getChunkAt(this.clampY(this.getTargetBlock().getX() - CHUNK_SIZE, 1, this.getTargetBlock().getZ())), v);
+        this.flatOcean(this.getWorld().getChunkAt(this.clampY(this.getTargetBlock().getX() - CHUNK_SIZE, 1, this.getTargetBlock().getZ() - CHUNK_SIZE)), v);
+        this.flatOcean(this.getWorld().getChunkAt(this.clampY(this.getTargetBlock().getX(), 1, this.getTargetBlock().getZ() - CHUNK_SIZE)), v);
+        this.flatOcean(this.getWorld().getChunkAt(this.clampY(this.getTargetBlock().getX() + CHUNK_SIZE, 1, this.getTargetBlock().getZ() - CHUNK_SIZE)), v);
     }
 
     @Override
     public final void info(final Message vm)
     {
         vm.brushName(this.getName());
-        vm.custom(ChatColor.BLUE + "Move selection blockPositionY " + ChatColor.GOLD + "x:" + this.moveDirections[0] + " y:" + this.moveDirections[1] + " z:" + this.moveDirections[2]);
+        vm.custom(ChatColor.RED + "THIS BRUSH DOES NOT UNDO");
+        vm.custom(ChatColor.GREEN + "Water level set to " + this.waterLevel);
+        vm.custom(ChatColor.GREEN + "Ocean floor level set to " + this.floorLevel);
     }
 
     @Override
@@ -212,149 +90,44 @@ public class MoveBrush extends Brush
     {
         for (int i = 1; i < par.length; i++)
         {
-            if (par[i].equalsIgnoreCase("info"))
-            {
-                v.getVoxelMessage().custom(ChatColor.GOLD + this.getName() + " Parameters:");
-                v.getVoxelMessage().custom(ChatColor.AQUA + "/b mv x[int] -- set the x direction (positive => east)");
-                v.getVoxelMessage().custom(ChatColor.AQUA + "/b mv y[int] -- set the y direction (positive => up)");
-                v.getVoxelMessage().custom(ChatColor.AQUA + "/b mv z[int] -- set the z direction (positive => south)");
-                v.getVoxelMessage().custom(ChatColor.AQUA + "/b mv reset -- reset the brush (x:0 y:0 z:0)");
-                v.getVoxelMessage().custom(ChatColor.AQUA + "Use arrow and gunpowder to define two points.");
-            }
-            if (par[i].equalsIgnoreCase("reset"))
-            {
-                this.moveDirections[0] = 0;
-                this.moveDirections[1] = 0;
-                this.moveDirections[2] = 0;
-                v.getVoxelMessage().custom(ChatColor.AQUA + "X direction set to: " + this.moveDirections[0]);
-                v.getVoxelMessage().custom(ChatColor.AQUA + "Y direction set to: " + this.moveDirections[1]);
-                v.getVoxelMessage().custom(ChatColor.AQUA + "Z direction set to: " + this.moveDirections[2]);
-            }
-            if (par[i].toLowerCase().startsWith("x"))
-            {
-                this.moveDirections[0] = Integer.valueOf(par[i].substring(1));
-                v.getVoxelMessage().custom(ChatColor.AQUA + "X direction set to: " + this.moveDirections[0]);
-            }
-            else if (par[i].toLowerCase().startsWith("y"))
-            {
-                this.moveDirections[1] = Integer.valueOf(par[i].substring(1));
-                v.getVoxelMessage().custom(ChatColor.AQUA + "Y direction set to: " + this.moveDirections[1]);
-            }
-            else if (par[i].toLowerCase().startsWith("z"))
-            {
-                this.moveDirections[2] = Integer.valueOf(par[i].substring(1));
-                v.getVoxelMessage().custom(ChatColor.AQUA + "Z direction set to: " + this.moveDirections[2]);
-            }
-        }
-    }
+            final String parameter = par[i];
 
-    /**
-     * Selection Helper class.
-     *
-     * @author MikeMatrix
-     */
-    private class Selection
-    {
-        /**
-         * Maximum amount of Blocks allowed blockPositionY the Selection.
-         */
-        private static final int MAX_BLOCK_COUNT = 5000000;
-        /**
-         * Calculated BlockStates of the selection.
-         */
-        private final ArrayList<BlockState> blockStates = new ArrayList<BlockState>();
-        /**
-         *
-         */
-        private Location location1 = null;
-        /**
-         *
-         */
-        private Location location2 = null;
-
-        /**
-         * Calculates region, then saves all Blocks as BlockState.
-         *
-         * @return boolean success.
-         * @throws Exception Message to be sent to the player.
-         */
-        public boolean calculateRegion() throws Exception
-        {
-            if (this.location1 != null && this.location2 != null)
+            if (parameter.equalsIgnoreCase("info"))
             {
-                if (this.location1.getWorld().equals(this.location2.getWorld()))
+                v.sendMessage(ChatColor.GREEN + "yo[number] to set the Level to which the water will rise.");
+                v.sendMessage(ChatColor.GREEN + "yl[number] to set the Level to which the ocean floor will rise.");
+            }
+            if (parameter.startsWith("yo"))
+            {
+                int newWaterLevel = Integer.parseInt(parameter.replace("yo", ""));
+                if (newWaterLevel < this.floorLevel)
                 {
-                    final int lowX = ((this.location1.getBlockX() <= this.location2.getBlockX()) ? this.location1.getBlockX() : this.location2.getBlockX());
-                    final int lowY = (this.location1.getBlockY() <= this.location2.getBlockY()) ? this.location1.getBlockY() : this.location2.getBlockY();
-                    final int lowZ = (this.location1.getBlockZ() <= this.location2.getBlockZ()) ? this.location1.getBlockZ() : this.location2.getBlockZ();
-                    final int highX = (this.location1.getBlockX() >= this.location2.getBlockX()) ? this.location1.getBlockX() : this.location2.getBlockX();
-                    final int highY = (this.location1.getBlockY() >= this.location2.getBlockY()) ? this.location1.getBlockY() : this.location2.getBlockY();
-                    final int highZ = (this.location1.getBlockZ() >= this.location2.getBlockZ()) ? this.location1.getBlockZ() : this.location2.getBlockZ();
-                    if (Math.abs(highX - lowX) * Math.abs(highZ - lowZ) * Math.abs(highY - lowY) > Selection.MAX_BLOCK_COUNT)
-                    {
-                        throw new Exception(ChatColor.RED + "Selection size above hardcoded limit, please use a smaller selection.");
-                    }
-                    final World world = this.location1.getWorld();
-                    for (int y = lowY; y <= highY; y++)
-                    {
-                        for (int x = lowX; x <= highX; x++)
-                        {
-                            for (int z = lowZ; z <= highZ; z++)
-                            {
-                                this.blockStates.add(world.getBlockAt(x, y, z).getState());
-                            }
-                        }
-                    }
-                    return true;
+                    newWaterLevel = this.floorLevel + 1;
                 }
+                this.waterLevel = newWaterLevel;
+                v.sendMessage(ChatColor.GREEN + "Water Level set to " + this.waterLevel);
             }
-            return false;
-        }
-
-        /**
-         * @return ArrayList<BlockState> calculated BlockStates of defined region.
-         */
-        public ArrayList<BlockState> getBlockStates()
-        {
-            return this.blockStates;
-        }
-
-        /**
-         * @return Location
-         */
-        public Location getLocation1()
-        {
-            return this.location1;
-        }
-
-        /**
-         * @param location1
-         */
-        public void setLocation1(final Location location1)
-        {
-            this.location1 = location1;
-        }
-
-        /**
-         * @return Location
-         */
-        public Location getLocation2()
-        {
-            return this.location2;
-        }
-
-        /**
-         * @param location2
-         */
-        public void setLocation2(final Location location2)
-        {
-            this.location2 = location2;
+            else if (parameter.startsWith("yl"))
+            {
+                int newFloorLevel = Integer.parseInt(parameter.replace("yl", ""));
+                if (newFloorLevel > this.waterLevel)
+                {
+                    newFloorLevel = this.waterLevel - 1;
+                    if (newFloorLevel == 0)
+                    {
+                        newFloorLevel = 1;
+                        this.waterLevel = 2;
+                    }
+                }
+                this.floorLevel = newFloorLevel;
+                v.sendMessage(ChatColor.GREEN + "Ocean floor Level set to " + this.floorLevel);
+            }
         }
     }
 
     @Override
     public String getPermissionNode()
     {
-        return "voxelsniper.brush.move";
+        return "voxelsniper.brush.flatocean";
     }
 }
