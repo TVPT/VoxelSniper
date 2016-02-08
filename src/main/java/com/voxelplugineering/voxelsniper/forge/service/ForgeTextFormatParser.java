b56321ef1495cc6d267ba174c00c379601844279
Nick Conde <nick@budgiehouse.com>
2016-02-08 13:28:00 -0800

    Fix forge shadowJar

--- src/main/java/com/voxelplugineering/voxelsniper/forge/service/ForgeTextFormatParser.java
+++ src/main/java/com/voxelplugineering/voxelsniper/forge/service/ForgeTextFormatParser.java
@@ -45,53 +45,7 @@ public class ForgeTextFormatParser extends AbstractService implements TextFormat
     @Override
     public String getFormat(TextFormat format)
     {
-        switch (format)
-        {
-        case BLACK:
-            return "&0";
-        case DARK_BLUE:
-            return "&1";
-        case DARK_GREEN:
-            return "&2";
-        case DARK_AQUA:
-            return "&3";
-        case DARK_RED:
-            return "&4";
-        case DARK_PURPLE:
-            return "&5";
-        case GOLD:
-            return "&6";
-        case GRAY:
-            return "&7";
-        case DARK_GRAY:
-            return "&8";
-        case BLUE:
-            return "&9";
-        case GREEN:
-            return "&a";
-        case AQUA:
-            return "&0b";
-        case RED:
-            return "&c";
-        case LIGHT_PURPLE:
-            return "&d";
-        case YELLOW:
-            return "&e";
-        case WHITE:
-            return "&f";
-        case BOLD:
-            return "&l";
-        case STRIKETHROUGH:
-            return "&m";
-        case UNDERLINE:
-            return "&n";
-        case ITALIC:
-            return "&o";
-        case RESET:
-            return "&r";
-        default:
-            return "&k";
-        }
+        return "";
     }
 
     @Override
